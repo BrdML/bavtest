70ad6fc1ccc5fbc877630d2487003bb1bcb02ff0
brd310 <bert.m310@gmail.com>
2021-10-29 23:15:34 +0200

    connectpagedone
    Page inscription:
    envoie de l'email et mot de passe a la base de donnée

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,24 +1,88 @@
-import React from 'react'
-import '../styles/form.css'
+import { React, useState } from 'react';
+import '../styles/form.css';
 import Button from '@material-ui/core/Button';
+import { useHistory } from 'react-router-dom';
 
-function connexion() {
+function Connexion() {
+
+    const history = useHistory()
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [isSubmitting, setIsSubmitting] = useState(false)
+    const { login } = useAuth()
+
+    const faildMessage = (message) => {
+        let formMess = document.querySelector('.form-message');
+
+        formMess.innerHTML = message;
+        formMess.style.opacity = '1';
+        setTimeout(() => {
+            formMess.style.opacity = '0';
+        }, 5000);
+        formMess.style.bacground = 'rgb(253,87,87)';
+        formMess.style.animation = 'dongle 1s';
+        setTimeout(() => {
+            formMess.style.animation = 'none'
+        }, 1000);
+    }
+
     return (
-        <form action="" className="form-content flex align-items-center">
-            <div className="form mx-auto flex align-items-center">
-                <h3 className="form-title text-white py-8 font-bold">Connectez vous</h3>
+        <form
+            className="form-content flex align-items-center"
+            onSubmit={async e => {
+                e.preventDefault()
+                //login logic
+                // console.log(email, password)
+                if (!email || !password) {
+                    faildMessage("Erreur");
+                }
+                setIsSubmitting(true)
+                successMessage("Vous ête Inscrit")
+                login(email, password)
+                    .then((response) => console.log(response))
+                    .catch((error) => {
+                        console.log(error.message)
+                        faildMessage(error.message);
+                    }).finally(() => setIsSubmitting(false));
+            }}
+        >
+            <div className="form2 mx-auto flex align-items-center">
+                <h3 className="form-title text-white py-8 font-bold">Inscrivez vous</h3>
                 <div className="form-group">
                     <label className="text-white font-bold" htmlFor="email">Email: </label>
-                    <input type="email" name="email" id="email" />
+                    <input
+                        value={email}
+                        type="email"
+                        name="email"
+                        id="email"
+                        onChange={e => setEmail(e.target.value)}
+                        required
+                    />
                 </div>
                 <div className="form-group">
-                    <label className="text-white" htmlFor="password">Mot de passe: </label>
-                    <input type="password" name="password" id="password" />
+                    <label className="text-white font-bold" htmlFor="password">Mot de passe: </label>
+                    <input
+                        value={password}
+                        type="password"
+                        name="password"
+                        id="password"
+                        onChange={e => setPassword(e.target.value)}
+                        required
+                    />
                 </div>
-                <Button variant="contained" color="secondary">Connexion</Button>
+                <Button
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    isLoading={isSubmitting}
+                    onClick={() => history.push('/profile')}
+                >
+                    Connexion
+                </Button>
+                <div id="form-message" className="form-message mx-auto text-red-800"></div>
             </div>
         </form>
     )
 }
 
-export default connexion
+export default Connexion
---
+++ src/pages/profile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useAuth } from '../contexts/AuthContext';
+
+export default function Profile() {
+    const { currentUser } = useAuth()
+
+    return (
+        <p className="text-black"> {`Bienvenue : ${currentUser}`}</p>
+    )
+}
