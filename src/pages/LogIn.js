fc397fdbef0b46e1eda96cd5d323bf062e41b748
brd310 <bert.m310@gmail.com>
2021-10-29 16:16:38 +0200

    Login&Signup

---
+++ src/pages/LogIn.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../styles/form.css'
+import Button from '@material-ui/core/Button';
+
+function connexion() {
+    return (
+        <form action="" className="form-content flex align-items-center">
+            <div className="form mx-auto flex align-items-center">
+                <h3 className="form-title text-white py-8 font-bold">Connectez vous</h3>
+                <div className="form-group">
+                    <label htmlFor="email">Email: </label>
+                    <input type="email" name="email" id="email" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Mot de passe: </label>
+                    <input type="password" name="password" id="password" />
+                </div>
+                <Button variant="contained" color="secondary">Connexion</Button>
+            </div>
+        </form>
+    )
+}
+
+export default connexion
